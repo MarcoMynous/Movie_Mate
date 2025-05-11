1b016ac705f12b900fc69129de08abaed019dd68
Marco_Mynous <ogochukwuulonnam986@gmail.com>
2025-05-11 12:34:07 +0100

    deploy config0

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-export { default as Search } from "./Search";
+export { default as Search } from "./search";
 export { default as MovieDetail } from "./MovieDetail";  
 export { default as MovieList } from "./MovieList";
 export { default as PageNotFound } from "./PageNotFound";
